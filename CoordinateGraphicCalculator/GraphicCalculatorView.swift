3e98776611b9d2fad91e3985bf3e10c1f8b83390
JiaYanwei <Jovi@Jia-3.local>
2017-07-28 17:53:08 +0800

    sync github with CoordinateGraphicCalculator project

---
+++ CoordinateGraphicCalculator/GraphicCalculatorView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphicCalculatorView.swift
+//  CoordinateGraphicCalculator
+//
+//  Created by JiaYanwei on 7/28/17.
+//  Copyright © 2017 JiaYanwei. All rights reserved.
+//
+
+import UIKit
+
+class GraphicCalculatorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
